3b822832ac2c1acfaf175d6ef9cdc7d13db58560
CelsoPontes <celsocomcarinho@gmail.com>
2021-05-30 12:44:02 -0300

    +RelativeLayout; +fab; +formulario_personagem;

---
+++ app/src/main/java/com/example/appmobileagenda/ui/activities/FormularioPersonagemActivity.java
@@ -0,0 +1,16 @@
+package com.example.appmobileagenda.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appmobileagenda.R;
+
+public class FormularioPersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_personagem);
+    }
+}
--- app/src/main/java/com/example/appmobileagenda/ui/activities/ListaPersonagemActivity.java
+++ app/src/main/java/com/example/appmobileagenda/ui/activities/ListaPersonagemActivity.java
@@ -1,27 +1,31 @@
-package com.example.appmobileagenda;
+package com.example.appmobileagenda.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.example.appmobileagenda.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaPersonagemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_personagem);
 
+        //lista de personagens
         List<String> personagem = new ArrayList<>(Arrays.asList("Alex", "Ken", "Ryu", "Guile"));
 
+        //referencia este listview com o activity_main_lista_personagem
         ListView listaDePersonagens = findViewById(R.id.activity_main_lista_personagem);
 
+        //relaciona um adapter com a lista personagem
         listaDePersonagens.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, personagem));
 
 //        TextView primeiroPersonagem = findViewById(R.id.textView1);
